30ae32182a0a63b26628c388db71a68499419ce5
caliandro-arturo <arturo.caliandro@mail.polimi.it>
2024-04-14 23:36:53 +0200

    add pins header

---
+++ src/pins.hpp
@@ -0,0 +1,38 @@
+/**
+ * This header contains the pin configuration of the robot.
+*/
+#ifndef PINS
+#define PINS
+
+#define LEFTARM
+#define RIGHTARM
+#define LEFTEAR
+#define RIGHTEAR
+#define HEADPIN
+#define BODYPIN
+
+#define PETSENSOR
+
+#define SLOT0
+#define SLOT1
+#define SLOT2
+#define SLOT3
+
+#define LED0_RED
+#define LED0_BLUE
+#define LED0_GREEN
+#define LED1_RED
+#define LED1_BLUE
+#define LED1_GREEN
+#define LED2_RED
+#define LED2_BLUE
+#define LED2_GREEN
+#define LED3_RED
+#define LED3_BLUE
+#define LED3_GREEN
+
+#define ENCODER_CW
+#define ENCODER_DT
+#define ENCODER_SW
+
+#endif
