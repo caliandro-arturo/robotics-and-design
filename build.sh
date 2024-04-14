6e809bcfd5d2dabd75da2eddce3be0a55a4d9717
caliandro-arturo <arturo.caliandro@mail.polimi.it>
2024-04-14 22:40:39 +0200

    use mega instead of uno in build configuration

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-arduino-cli compile --fqbn arduino:avr:uno "$1"
-arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno "$1"
+arduino-cli compile --fqbn arduino:avr:mega "$1"
+arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:mega "$1"
 
