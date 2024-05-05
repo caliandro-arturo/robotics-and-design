0257f59e33b70fdaadd1b3d3e84706905b1b5716
caliandro-arturo <arturo.caliandro@mail.polimi.it>
2024-05-05 20:13:52 +0200

    format sketch using clang configuration

--- robotics-and-design.ino
+++ robotics-and-design.ino
@@ -28,11 +28,21 @@ using namespace ace_routine;
 
 uint8_t currentEye[8];
 
-enum Mood {NORMAL, HAPPY, SAD, ANGRY, PISSED };
+enum Mood { NORMAL,
+            HAPPY,
+            SAD,
+            ANGRY,
+            PISSED };
 
 volatile Mood mood;
 
-enum Status {START, SET_MINUTES, SET_HOURS, PHONE_CHECK, TIMER_GOING, HAND_DETECTED, TIMER_FINISHED};
+enum Status { START,
+              SET_MINUTES,
+              SET_HOURS,
+              PHONE_CHECK,
+              TIMER_GOING,
+              HAND_DETECTED,
+              TIMER_FINISHED };
 
 volatile Status status;
 
@@ -47,44 +57,43 @@ volatile Status status;
 */
 
 
-typedef struct{
-  int pin;
-  bool phoneIn; 
-  //RGB_Led rgbled;
-}PhoneSlot;
+typedef struct {
+        int pin;
+        bool phoneIn;
+        //RGB_Led rgbled;
+} PhoneSlot;
 
 PhoneSlot phoneSlot[1];
 
 
-void init_phone_slots(){
-  phoneSlot[0].pin = SLOT0; 
-  /*phoneSlot[1].pin = SLOT1;
+void init_phone_slots() {
+    phoneSlot[0].pin = SLOT0;
+    /*phoneSlot[1].pin = SLOT1;
   phoneSlot[2].pin = SLOT2;
   phoneSlot[3].pin = SLOT3;*/
- /* phonePresent = false;
+    /* phonePresent = false;
   for(int i=0; i<1; i++){
     phoneSlot[i].phoneIn = false;
   }
   init_leds();*/
-
 }
 
 //may be the opposite
-void check_phone(){
-  int count = 0;
-  //for(int i= 0; i<1; i++){
-    if(digitalRead(phoneSlot[0].pin) == LOW){ //detected
-      phonePresent = true;
-      phoneSlot[0].phoneIn = true;
-      count++;
+void check_phone() {
+    int count = 0;
+    //for(int i= 0; i<1; i++){
+    if (digitalRead(phoneSlot[0].pin) == LOW) {  //detected
+        phonePresent = true;
+        phoneSlot[0].phoneIn = true;
+        count++;
 
-    }else{
-      phonePresent = false;
-      phoneSlot[0].phoneIn = false;
-    }  
- // }
- // if (count>0)
-  //  phonePresent = true;
+    } else {
+        phonePresent = false;
+        phoneSlot[0].phoneIn = false;
+    }
+    // }
+    // if (count>0)
+    //  phonePresent = true;
 }
 
 
@@ -110,107 +119,105 @@ unsigned long interval = 1000;
 volatile int lastEncoded = 0;
 unsigned long currentMillis;
 
-void reset_encoder(){
-  encoderPos = 0;
+void reset_encoder() {
+    encoderPos = 0;
 }
 
-void increment_hours(){
-  hour =  (encoderPos % 30)/10 ;
+void increment_hours() {
+    hour = (encoderPos % 30) / 10;
 }
 
-void increment_minutes(){
-  minute = (encoderPos % 300)/5;
+void increment_minutes() {
+    minute = (encoderPos % 300) / 5;
 }
 
 
-void increment_status(){   
-  unsigned long timeNow = millis();
-  if (timeNow - lastTime > DEBOUNCING_PERIOD){
-    if(status == START)
-      status = SET_MINUTES;
-    else if (status == SET_MINUTES){
-      status = SET_HOURS;
-      //setMinute = minute;
+void increment_status() {
+    unsigned long timeNow = millis();
+    if (timeNow - lastTime > DEBOUNCING_PERIOD) {
+        if (status == START)
+            status = SET_MINUTES;
+        else if (status == SET_MINUTES) {
+            status = SET_HOURS;
+            //setMinute = minute;
+        }
+        lastTime = timeNow;
     }
-    lastTime = timeNow;
-  }
 }
 
 void encoderISR() {
-  int MSB = digitalRead(ENCODER_CLK);
-  int LSB = digitalRead(ENCODER_DT);
+    int MSB = digitalRead(ENCODER_CLK);
+    int LSB = digitalRead(ENCODER_DT);
 
-  int encoded = (MSB << 1) | LSB;
-  int sum = (lastEncoded << 2) | encoded;
+    int encoded = (MSB << 1) | LSB;
+    int sum = (lastEncoded << 2) | encoded;
 
-  if (sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011) {
-    encoderPos++;
-  } else if (sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000) {
-    encoderPos--;
-  }
+    if (sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011) {
+        encoderPos++;
+    } else if (sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000) {
+        encoderPos--;
+    }
 
-  lastEncoded = encoded;
+    lastEncoded = encoded;
 }
 
 
 void clock_setup() {
-  counter=0;
-  display.setBrightness(7);
-  display.clear();
-  minute = 0;
-  hour = 0;
-  isMinuteSet = false;
-  isHourSet = false;
-  pinMode(ENCODER_CLK, INPUT_PULLUP);         //encoder input setup
-  pinMode(ENCODER_DT, INPUT_PULLUP);
-  pinMode(ENCODER_SW, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(ENCODER_SW), increment_status, RISING); 
-  attachInterrupt(digitalPinToInterrupt(ENCODER_CLK),encoderISR, CHANGE);
-
+    counter = 0;
+    display.setBrightness(7);
+    display.clear();
+    minute = 0;
+    hour = 0;
+    isMinuteSet = false;
+    isHourSet = false;
+    pinMode(ENCODER_CLK, INPUT_PULLUP);  //encoder input setup
+    pinMode(ENCODER_DT, INPUT_PULLUP);
+    pinMode(ENCODER_SW, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(ENCODER_SW), increment_status, RISING);
+    attachInterrupt(digitalPinToInterrupt(ENCODER_CLK), encoderISR, CHANGE);
 }
 
 
 void countdown() {
-  currentMillis = millis();
-
-  if (hour > 0 || minute > 0) {
-    display.showNumberDecEx(100 * hour + minute, 0b01000000, true);
-    if (currentMillis - previousMillis >= 60000) {
-      previousMillis = currentMillis;
-      Serial.println("we");
-      if (minute > 0) {
-        minute--;
-        display.showNumberDecEx(100*hour + minute, 0b01000000, true);
-      } else {
-        if (hour > 0) {
-          hour--;
-          minute = 59;
-          display.showNumberDecEx(100*hour + minute, 0b01000000, true);
-        }
-      }
-    }
-
-    //TO BE SPECIFIED IF LEFT OR RIGHT
-    int proximity = digitalRead(LEFTPROX);
-    if(proximity ==  LOW){
-      mood = ANGRY;
-    }else{
-      mood = NORMAL;
-    }
-
     currentMillis = millis();
 
-  }else{ 
-    previousMillis = 0;
-    status = TIMER_FINISHED;
-   
-  }
+    if (hour > 0 || minute > 0) {
+        display.showNumberDecEx(100 * hour + minute, 0b01000000, true);
+        if (currentMillis - previousMillis >= 60000) {
+            previousMillis = currentMillis;
+            Serial.println("we");
+            if (minute > 0) {
+                minute--;
+                display.showNumberDecEx(100 * hour + minute, 0b01000000, true);
+            } else {
+                if (hour > 0) {
+                    hour--;
+                    minute = 59;
+                    display.showNumberDecEx(100 * hour + minute, 0b01000000, true);
+                }
+            }
+        }
+
+        //TO BE SPECIFIED IF LEFT OR RIGHT
+        int proximity = digitalRead(LEFTPROX);
+        if (proximity == LOW) {
+            mood = ANGRY;
+        } else {
+            mood = NORMAL;
+        }
+
+        currentMillis = millis();
+
+    } else {
+        previousMillis = 0;
+        status = TIMER_FINISHED;
+    }
 }
 
 
 
 /*
-* 
+*
 * EYES
 *
 *
@@ -227,7 +234,7 @@ uint8_t UP_RIGHT_EYE[] = {
     0b00000000
 };
 
-uint8_t  ANGRY_EYE[] = {
+uint8_t ANGRY_EYE[] = {
     0b00000000,
     0b00100000,
     0b01110000,
@@ -239,7 +246,7 @@ uint8_t  ANGRY_EYE[] = {
 };
 
 
-uint8_t  SAD_EYE[] = {
+uint8_t SAD_EYE[] = {
     0b00000000,
     0b00000100,
     0b00001110,
@@ -251,7 +258,7 @@ uint8_t  SAD_EYE[] = {
 };
 
 
-uint8_t  FRONT_EYE[] = {
+uint8_t FRONT_EYE[] = {
     0b00000000,
     0b00111100,
     0b01111110,
@@ -262,7 +269,7 @@ uint8_t  FRONT_EYE[] = {
     0b00000000
 };
 
-uint8_t  HAPPY_EYE[] = {
+uint8_t HAPPY_EYE[] = {
     0b00000000,
     0b00011000,
     0b00111100,
@@ -273,7 +280,7 @@ uint8_t  HAPPY_EYE[] = {
     0b00000000
 };
 
-uint8_t  PISSED_EYE[] = {
+uint8_t PISSED_EYE[] = {
     0b00000000,
     0b00000000,
     0b00000000,
@@ -285,102 +292,101 @@ uint8_t  PISSED_EYE[] = {
 };
 
 COROUTINE(blink) {
-  COROUTINE_LOOP() {
-      close_eyes();
-      COROUTINE_DELAY(50);
-      front_eyes(currentEye);
-      COROUTINE_DELAY(1500);
-  }
+    COROUTINE_LOOP() {
+        close_eyes();
+        COROUTINE_DELAY(50);
+        front_eyes(currentEye);
+        COROUTINE_DELAY(1500);
+    }
 }
 
 
 MATRIX7219 screen = MATRIX7219(EYE_DIN, EYE_CS, EYE_CLK, 2);
 
 void print_matrix(uint8_t image[8], uint8_t display = 1) {
-  for (int i = 1; i <= 8; i++) {
-      screen.setRow(i, image[i - 1], display);
-  }
+    for (int i = 1; i <= 8; i++) {
+        screen.setRow(i, image[i - 1], display);
+    }
 }
 
 uint8_t reverse(uint8_t b) {
-  b = (b & 0xF0) >> 4 | (b & 0x0F) << 4;
-  b = (b & 0xCC) >> 2 | (b & 0x33) << 2;
-  b = (b & 0xAA) >> 1 | (b & 0x55) << 1;
-  return b;
+    b = (b & 0xF0) >> 4 | (b & 0x0F) << 4;
+    b = (b & 0xCC) >> 2 | (b & 0x33) << 2;
+    b = (b & 0xAA) >> 1 | (b & 0x55) << 1;
+    return b;
 }
 
 void close_eyes() {
-  for (int i = 1; i <= 4; i++) {
-      screen.setRow(i, 0, 1);
-      screen.setRow(8 - i + 1, 0, 1);
-      screen.setRow(i, 0, 2);
-      screen.setRow(8 - i + 1, 0, 2);
-      delay(30);
-  }
+    for (int i = 1; i <= 4; i++) {
+        screen.setRow(i, 0, 1);
+        screen.setRow(8 - i + 1, 0, 1);
+        screen.setRow(i, 0, 2);
+        screen.setRow(8 - i + 1, 0, 2);
+        delay(30);
+    }
 }
 
 void side_eyes(uint8_t EYE[]) {
-  for (int i = 4; i >= 1; i--) {
-      screen.setRow(i, EYE[i - 1], 1);
-      screen.setRow(8 - i + 1, EYE[8 - i], 1);
-      screen.setRow(i, EYE[i - 1], 2);
-      screen.setRow(8 - i + 1, EYE[8 - i], 2);
-      delay(30);
-  }
+    for (int i = 4; i >= 1; i--) {
+        screen.setRow(i, EYE[i - 1], 1);
+        screen.setRow(8 - i + 1, EYE[8 - i], 1);
+        screen.setRow(i, EYE[i - 1], 2);
+        screen.setRow(8 - i + 1, EYE[8 - i], 2);
+        delay(30);
+    }
 }
 
 void front_eyes(uint8_t EYE[]) {
-  for (int i = 4; i >= 1; i--) {
-      screen.setRow(i, EYE[i - 1], 1);
-      screen.setRow(8 - i + 1, EYE[8 - i], 1);
-      screen.setRow(i, reverse(EYE[i - 1]), 2);
-      screen.setRow(8 - i + 1, reverse(EYE[8 - i]), 2);
-      delay(30);
-  }
+    for (int i = 4; i >= 1; i--) {
+        screen.setRow(i, EYE[i - 1], 1);
+        screen.setRow(8 - i + 1, EYE[8 - i], 1);
+        screen.setRow(i, reverse(EYE[i - 1]), 2);
+        screen.setRow(8 - i + 1, reverse(EYE[8 - i]), 2);
+        delay(30);
+    }
 }
 
 
-void assign_eye(uint8_t EYE[8]){
-  for(int i = 0; i<8; i++){
-    currentEye[i] = EYE[i];
-  }
+void assign_eye(uint8_t EYE[8]) {
+    for (int i = 0; i < 8; i++) {
+        currentEye[i] = EYE[i];
+    }
 }
 
-void blink_happy(){
-  close_eyes();
-  delay(50);
-  front_eyes(HAPPY_EYE);
-  delay(1000);
+void blink_happy() {
+    close_eyes();
+    delay(50);
+    front_eyes(HAPPY_EYE);
+    delay(1000);
 }
 
 
 void eye_setup() {
-  screen.begin();
-  screen.setBrightness(1);
-  screen.clear();
+    screen.begin();
+    screen.setBrightness(1);
+    screen.clear();
 }
 
 
 
-void assign_mood(){
-  switch(mood){
-    case HAPPY:
-    assign_eye(HAPPY_EYE);
-    break;
-    case NORMAL:
-    assign_eye(FRONT_EYE);
-    break;
-    case ANGRY:
-    assign_eye(ANGRY_EYE);
-    break;
-    case SAD:
-    assign_eye(SAD_EYE);
-    break;
-    case PISSED:
-    assign_eye(PISSED_EYE);
-    break;
-  }
-
+void assign_mood() {
+    switch (mood) {
+        case HAPPY:
+            assign_eye(HAPPY_EYE);
+            break;
+        case NORMAL:
+            assign_eye(FRONT_EYE);
+            break;
+        case ANGRY:
+            assign_eye(ANGRY_EYE);
+            break;
+        case SAD:
+            assign_eye(SAD_EYE);
+            break;
+        case PISSED:
+            assign_eye(PISSED_EYE);
+            break;
+    }
 }
 
 
@@ -392,79 +398,78 @@ void assign_mood(){
 *
 */
 
-void setup(){
-  status = START;
-  mood = NORMAL;
-  eye_setup();
-  clock_setup();
-  pinMode(LEFTPROX, INPUT_PULLUP); 
-  pinMode(RIGHTPROX, INPUT_PULLUP); 
-  init_phone_slots();
-  Serial.begin(9600);
+void setup() {
+    status = START;
+    mood = NORMAL;
+    eye_setup();
+    clock_setup();
+    pinMode(LEFTPROX, INPUT_PULLUP);
+    pinMode(RIGHTPROX, INPUT_PULLUP);
+    init_phone_slots();
+    Serial.begin(9600);
 }
 
-void loop(){
-  switch(status){
+void loop() {
+    switch (status) {
 
-    case START: 
-      increment_minutes();
-      display.showNumberDecEx(minute, 0b01000000, true);
-      break;
+        case START:
+            increment_minutes();
+            display.showNumberDecEx(minute, 0b01000000, true);
+            break;
 
-    case SET_MINUTES:
-      if(isMinuteSet==0 && isHourSet==0){
-        reset_encoder();
-        Serial.println("Minutes set");
-        isMinuteSet = true;
-        setMinute = minute;
-        display.showNumberDecEx(setMinute, 0b01000000, true);
-        blink_happy();
-      }
-      increment_hours();
-      display.showNumberDecEx(100*hour + minute, 0b01000000, true);
-      break;
+        case SET_MINUTES:
+            if (isMinuteSet == 0 && isHourSet == 0) {
+                reset_encoder();
+                Serial.println("Minutes set");
+                isMinuteSet = true;
+                setMinute = minute;
+                display.showNumberDecEx(setMinute, 0b01000000, true);
+                blink_happy();
+            }
+            increment_hours();
+            display.showNumberDecEx(100 * hour + minute, 0b01000000, true);
+            break;
 
-    case SET_HOURS:
-      if(isMinuteSet==1 && isHourSet==0){
-        Serial.println("Hours set");
-        isHourSet = true;
-        setHour = hour;
-        display.showNumberDecEx(100*setHour + setMinute, 0b01000000, true);
-        blink_happy();
-        isHourSet = true;
-        status = PHONE_CHECK;
-      }
-      break;
+        case SET_HOURS:
+            if (isMinuteSet == 1 && isHourSet == 0) {
+                Serial.println("Hours set");
+                isHourSet = true;
+                setHour = hour;
+                display.showNumberDecEx(100 * setHour + setMinute, 0b01000000, true);
+                blink_happy();
+                isHourSet = true;
+                status = PHONE_CHECK;
+            }
+            break;
 
-    case PHONE_CHECK:
-      mood = NORMAL;
-      check_phone();
-      if(phonePresent == true & isMinuteSet==1 && isHourSet==1){
-        Serial.println("Timer starts!");
-        isMinuteSet = 0;
-        isHourSet = 0;
-        status = TIMER_GOING;
-      }
-      break;
+        case PHONE_CHECK:
+            mood = NORMAL;
+            check_phone();
+            if (phonePresent == true & isMinuteSet == 1 && isHourSet == 1) {
+                Serial.println("Timer starts!");
+                isMinuteSet = 0;
+                isHourSet = 0;
+                status = TIMER_GOING;
+            }
+            break;
 
-    case TIMER_GOING:
-      countdown();
-      break;
-  
-    case TIMER_FINISHED:
-      Serial.println("Time finished!");
-      mood = NORMAL;
-      isMinuteSet = false;
-      isHourSet = false;
-      minute = 0;
-      setHour = 0;
-      setMinute = 0;
-      reset_encoder();
-      display.showNumberDecEx(0x00, 0b01000000, true);
-      status = START;
-      break;
-  }
-  assign_mood();
-  blink.runCoroutine();
+        case TIMER_GOING:
+            countdown();
+            break;
 
+        case TIMER_FINISHED:
+            Serial.println("Time finished!");
+            mood = NORMAL;
+            isMinuteSet = false;
+            isHourSet = false;
+            minute = 0;
+            setHour = 0;
+            setMinute = 0;
+            reset_encoder();
+            display.showNumberDecEx(0x00, 0b01000000, true);
+            status = START;
+            break;
+    }
+    assign_mood();
+    blink.runCoroutine();
 }
